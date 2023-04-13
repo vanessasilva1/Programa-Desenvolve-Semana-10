0a01d6ac000c284935099d0f608984d4418a1bc8
Vanessa <vanessa35lv@gmail.com>
2023-04-13 14:46:26 -0300

    exemplos da aula 1

---
+++ tipo-boolean.js
@@ -0,0 +1,13 @@
+const num1 = 5;
+const num2 = 4;
+console.log(num1 === num2);
+
+const num3 = 5;
+const num4 = 5;
+console.log(num3 === num4);
+
+ let cadastroAtivo = true;
+
+ const nome1 = 'Alura';
+ const nome2 =  'a';
+ console.log(nome1 == nome2);
