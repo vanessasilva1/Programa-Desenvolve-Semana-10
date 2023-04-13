830ddb22974cf4a15d6925eebbc129bc653818a3
Vanessa <vanessa35lv@gmail.com>
2023-04-13 16:16:48 -0300

    Aprendendo sobre truthy e falsy

---
+++ truthy-falsy.js
@@ -0,0 +1,24 @@
+//boolean
+const usuarioLogado = true;
+const contaPaga = false;
+
+//truthy ou falsy
+
+// 0 - false
+// 1 - true
+
+console.log(0 == false); // == compara os valores. === compara os valores e os tipos
+console.log("" == false);
+console.log(1 == true);
+
+//null - vazio ou nada
+//undefined
+
+let minhaVar;
+let varNull = null;
+
+console.log(minhaVar);
+console.log(varNull);
+
+console.log(typeof minhaVar);
+console.log(typeof varNull);
