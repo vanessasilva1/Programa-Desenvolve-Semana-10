64b5da27d24e20d113256c7ad286eaadc2ce7332
Vanessa <vanessa35lv@gmail.com>
2023-04-13 14:05:19 -0300

    commit

---
+++ tipo-number.js
@@ -0,0 +1,10 @@
+const num1 = 6;
+const num2 = 10;
+
+const soma = num1 + num2;
+console.log(soma);
+
+const texto = 'OI';
+const numero = 3;
+const NaNsoma = texto * numero;
+console.log(NaNsoma);
---
+++ tipo-string.js
@@ -0,0 +1,7 @@
+const citacao = 'Segundo Café (2015), as "bibliotecas escolares..."';
+console.log(citacao);
+
+const nome1 = "Olá, ";
+const nome2 = "Mundo";
+const concatenacao = nome1+ nome2;
+console.log(concatenacao);
