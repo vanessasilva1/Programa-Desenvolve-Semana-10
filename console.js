dd9c78df2685640918a6032d33e2dfbf8f29a4d9
Vanessa <vanessa35lv@gmail.com>
2023-04-14 16:08:24 -0300

    JavaScript

---
+++ console.js
@@ -0,0 +1,13 @@
+console.log(123);
+
+// tratamento de erros - pesquisar
+
+console.error('deu erro!')
+
+/*console.error() para exibir mensagens de erro;
+console.table() para visualizar de forma mais organizada informações tabulares;
+console.time() e console.timeEnd() para temporizar período que uma operação de código leva para ser iniciada e concluída;
+console.trace() para exibir a stacktrace de todos os pontos (ou seja, os arquivos chamados) por onde o código executado passou durante a execução.*/
+
+console.log("deu erro");
+console.error(new Error("deu erro"));
---
+++ operadores-de-comparacao.js
@@ -0,0 +1,21 @@
+// == comparacão implícita
+
+const numero = 1;
+const texto = '1';
+
+console.log(numero == texto);
+
+console.log(numero === texto);
+
+//typeof
+
+console.log(typeof numero);
+console.log(typeof texto);
+
+// == só compara o valor
+// === RECOMENDADO. Boas práticas. Compara o valor e o tipo de dado
+
+//conversão explícita
+
+Number();
+String();
