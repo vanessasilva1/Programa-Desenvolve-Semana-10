290502e582392908ebbb35cccc628e92b2b53afc
Vanessa <vanessa35lv@gmail.com>
2023-04-14 16:08:30 -0300

    Operador ternário

---
+++ operador-ternario.js
@@ -0,0 +1,12 @@
+idadeMinima = 18;
+idadeCliente = 17;
+
+if(idadeCliente >= idadeMinima) {
+    console.log('Pode beber');
+}
+else {
+    console.log('Não pode beber');
+}
+            // condição                   //true     //false
+console.log(idadeCliente >= idadeMinima ? "Beber" : "Não beber");
+
